e1a611e5b58249b88bca7b96f091919c70088dd4
Haemin Kim <148521537+minixzip@users.noreply.github.com>
2025-04-10 13:24:53 +0900

    [Bronze V] Title: A+B - 7, Time: 96 ms, Memory: 9512 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,21 @@
+function main() {
+  const data = getData();
+
+  for (let i=1; i<data.length; i++){
+    console.log(`Case #${i}: ${data[i][0] + data[i][1]}`);
+  }
+}
+main();
+
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString();
+  const arr = fileData.trim().split("\n");
+  const result = [];
+  for (let row of arr) {
+    const rowArr = row.split(' ');
+    for (let k=0; k<rowArr.length; k++) rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    result.push(rowArr);
+  }
+  return result;
+}
